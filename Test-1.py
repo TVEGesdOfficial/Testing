34da991800ba3f55feeb77e4b7690c774ee5ec05
TVEGesd <133129617+TVEGesdOfficial@users.noreply.github.com>
2024-10-07 14:14:43 -0500

    Create Test-1.py

---
+++ Test-1.py
@@ -0,0 +1,9 @@
+def check_even_odd(num):
+  if num % 2 == 0:
+    return f'{num} is enven'
+  else:
+    return f'{num} is odd'
+
+# Test the function
+print(check_even_odd(4))
+print(check_even_odd(7))
